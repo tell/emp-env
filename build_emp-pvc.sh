d8b8ace73acf7e7f99843ad9775f4382cb206ca4
Tadanori TERUYA <tadanori.teruya@gmail.com>
2019-04-25 00:52:48 +0900

    update

--- build_emp-pvc.sh
+++ build_emp-pvc.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -ev
 
+if [ $# -gt 0 ]; then
+	echo 'Run in checking mode.'
+	check_mode=1
+fi
+
 rm -rf emp-env
 mkdir emp-env
 export ENV_PATH=$(realpath emp-env)
@@ -8,6 +13,7 @@ export ENV_PATH=$(realpath emp-env)
 rm -rf relic
 git clone https://github.com/relic-toolkit/relic.git
 pushd relic
+git log -1 | cat
 cmake -DCMAKE_INSTALL_PREFIX=$ENV_PATH \
 	-DALIGN=16 -DARCH=X64 -DARITH=curve2251-sse -DCHECK=off -DFB_POLYN=251 -DFB_METHD="INTEG;INTEG;QUICK;QUICK;QUICK;QUICK;LOWER;SLIDE;QUICK" -DFB_PRECO=on -DFB_SQRTF=off -DEB_METHD="PROJC;LODAH;COMBD;INTER" -DEC_METHD="CHAR2" -DCOMP="-O3 -funroll-loops -fomit-frame-pointer -march=native -msse4.2 -mpclmul" -DTIMER=CYCLE -DWITH="MD;DV;BN;FB;EB;EC" -DWSIZE=64 \
 	. 2>&1 > /dev/null
@@ -17,8 +23,11 @@ popd
 
 rm -rf emp-tool
 git clone https://github.com/emp-toolkit/emp-tool.git
-./patch_emp-tool.sh
+if [ $check_mode -eq 1 ]; then
+	./patch_emp-tool.sh
+fi
 pushd emp-tool
+git log -1 | cat
 cmake -DCMAKE_INSTALL_PREFIX=$ENV_PATH \
 	-DCMAKE_C_FLAGS="-I$ENV_PATH/include -Wall -Wextra" \
 	-DCMAKE_CXX_STANDARD=11 \
@@ -31,8 +40,11 @@ popd
 
 rm -rf emp-ot
 git clone https://github.com/emp-toolkit/emp-ot.git
-./patch_emp-ot.sh
+if [ $check_mode -eq 1 ]; then
+	./patch_emp-ot.sh
+fi
 pushd emp-ot
+git log -1 | cat
 cmake -DCMAKE_INSTALL_PREFIX=$ENV_PATH \
 	-DCMAKE_C_FLAGS="-I$ENV_PATH/include -Wall -Wextra" \
 	-DCMAKE_CXX_STANDARD=11 \
@@ -44,8 +56,11 @@ popd
 
 rm -rf emp-pvc
 git clone https://github.com/emp-toolkit/emp-pvc.git
-./patch_emp-pvc.sh
+if [ $check_mode -eq 1 ]; then
+	./patch_emp-pvc.sh
+fi
 pushd emp-pvc
+git log -1 | cat
 cmake -DCMAKE_INSTALL_PREFIX=$ENV_PATH \
 	-DCMAKE_C_FLAGS="-I$ENV_PATH/include -Wall -Wextra" \
 	-DCMAKE_CXX_STANDARD=11 \
