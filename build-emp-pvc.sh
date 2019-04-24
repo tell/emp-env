b771d38d92cddbb65f30b515c505d44e44d2c849
Tadanori TERUYA <tadanori.teruya@gmail.com>
2019-04-24 18:29:37 +0900

    update

--- build-emp-pvc.sh
+++ build-emp-pvc.sh
@@ -11,7 +11,7 @@ pushd relic
 cmake -DCMAKE_INSTALL_PREFIX=$ENV_PATH \
 	-DALIGN=16 -DARCH=X64 -DARITH=curve2251-sse -DCHECK=off -DFB_POLYN=251 -DFB_METHD="INTEG;INTEG;QUICK;QUICK;QUICK;QUICK;LOWER;SLIDE;QUICK" -DFB_PRECO=on -DFB_SQRTF=off -DEB_METHD="PROJC;LODAH;COMBD;INTER" -DEC_METHD="CHAR2" -DCOMP="-O3 -funroll-loops -fomit-frame-pointer -march=native -msse4.2 -mpclmul" -DTIMER=CYCLE -DWITH="MD;DV;BN;FB;EB;EC" -DWSIZE=64 \
 	.
-make -j VERBOSE=1
+make -j
 make install
 popd
 
