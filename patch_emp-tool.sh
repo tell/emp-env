b88973fa859ea3603dea9b26fcf0c87a90677868
Tadanori TERUYA <tadanori.teruya@gmail.com>
2019-04-24 19:42:27 +0900

    update and add a patch script

--- patch_emp-tool.sh
+++ patch_emp-tool.sh
@@ -3,5 +3,6 @@ set -ev
 pushd emp-tool
 find . \( -iname '*.h' -or -iname '*.hpp' -or -iname '*.c' -or -iname '*.cpp' \) -print \
 	-exec sed -i -e 's/STS_OK/RLC_OK/g' -e 's/BN_POS/RLC_POS/g' -e 's/DIG_LOG/RLC_DIG_LOG/g' \
-		-e 's/SPLIT(\([[:alpha:]]*\), \([[:alpha:]]*\), \([[:alpha:]]*\), RLC_DIG_LOG)/RLC_RIP(\1, \2, \3)/g' {} \;
+		-e 's/SPLIT(\([[:alpha:]]*\), \([[:alpha:]]*\), \([[:alpha:]]*\), RLC_DIG_LOG)/RLC_RIP(\1, \2, \3)/g' \
+		{} \;
 popd
