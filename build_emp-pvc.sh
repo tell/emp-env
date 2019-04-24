badb117c76bb227708a47d10accd122734777175
Tadanori TERUYA <tadanori.teruya@gmail.com>
2019-04-24 19:54:12 +0900

    update and fix

--- build_emp-pvc.sh
+++ build_emp-pvc.sh
@@ -10,7 +10,7 @@ git clone https://github.com/relic-toolkit/relic.git
 pushd relic
 cmake -DCMAKE_INSTALL_PREFIX=$ENV_PATH \
 	-DALIGN=16 -DARCH=X64 -DARITH=curve2251-sse -DCHECK=off -DFB_POLYN=251 -DFB_METHD="INTEG;INTEG;QUICK;QUICK;QUICK;QUICK;LOWER;SLIDE;QUICK" -DFB_PRECO=on -DFB_SQRTF=off -DEB_METHD="PROJC;LODAH;COMBD;INTER" -DEC_METHD="CHAR2" -DCOMP="-O3 -funroll-loops -fomit-frame-pointer -march=native -msse4.2 -mpclmul" -DTIMER=CYCLE -DWITH="MD;DV;BN;FB;EB;EC" -DWSIZE=64 \
-	. > /dev/null
+	. 2>&1 > /dev/null
 make -j > /dev/null
 make install > /dev/null
 popd
@@ -23,9 +23,9 @@ cmake -DCMAKE_INSTALL_PREFIX=$ENV_PATH \
 	-DCMAKE_C_FLAGS="-I$ENV_PATH/include" \
 	-DCMAKE_CXX_FLAGS="-I$ENV_PATH/include" \
 	-DTHREADING=on \
-	.
-make -j
-make install
+	. 2>&1 > /dev/null
+make -j > /dev/null
+make install > /dev/null
 popd
 
 rm -rf emp-ot
@@ -35,14 +35,14 @@ pushd emp-ot
 cmake -DCMAKE_INSTALL_PREFIX=$ENV_PATH \
 	-DCMAKE_C_FLAGS="-I$ENV_PATH/include" \
 	-DCMAKE_CXX_FLAGS="-I$ENV_PATH/include" \
-	.
-make VERBOSE=1
-make install
+	. 2>&1 > /dev/null
+make -j > /dev/null
+make install > /dev/null
 popd
 
 rm -rf emp-pvc
 git clone https://github.com/emp-toolkit/emp-pvc.git
-pushd emp-ot
+pushd emp-pvc
 cmake -DCMAKE_INSTALL_PREFIX=$ENV_PATH \
 	-DCMAKE_C_FLAGS="-I$ENV_PATH/include" \
 	-DCMAKE_CXX_FLAGS="-I$ENV_PATH/include" \
